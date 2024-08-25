804fe249240058507aa37a8bf6517fb53c348ded
Florian Thiel <flth@flth.de>
2024-08-25 14:12:07 +0200

    initial

---
+++ strings.lua
@@ -0,0 +1,42 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Two-axle dump truck (Ommi 51/F-z 120)",
+		["mod_desc"] = "From 1952 onwards, almost 4,000 wagons of the Ommi 51 type were built and were in service until 2004. The wagons are suitable for bulk goods of various kinds. Its five troughs can be tilted to both sides and can discharge the bulk material directly into trucks or over bunker walls. Unloading was difficult and could only be done by trained operators, otherwise the wagon could tip over. In the 1990s, the wagons were temporarily on the road as service freight wagons.",
+
+        ["Ommi51_menu"] = "Two-axle dump truck",
+
+		["Ommi51_name"] = "Ommi 51 (1952)",
+		["Ommi51_description"] = "From 1952 almost 4,000 wagons of the type Ommi 51 were purchased by the Bundesbahn. The wagons are suitable for bulk goods of various kinds. Its five troughs can be tilted to either side and can discharge the bulk material directly into trucks or over bunker walls.",
+        ["Fz120_name"] = "F-z 120 (1965)",
+		["Fz120_description"] = "From 1965 the former Ommi 51 wagons were redesignated as F-z 120 according to RIV guidelines. The wagons are suitable for bulk goods of various kinds. Its five troughs can be tilted to both sides and can deliver the bulk material directly into trucks or over bunker walls.",
+        ["Fz120_DBAG_name"] = "F-z 120 DBAG (1994)",
+		["Fz120_DBAG_description"] = "Many wagons also came to the Deutsche Bahn AG and were still in the stock until 2004. The wagons are suitable for bulk goods of various kinds. Its five troughs can be tilted to either side and can deliver the bulk material directly into trucks or over bunker walls.",
+        ["Ballast Gravel165_name"] = "Schotter-Kies 165 (1991).",
+		["Schotterkies165_description"] = "From 1991, 400 F-z 120s are re-designated as service freight wagons and given the designation Schotter-Kies 165. In 1995, most of these wagons return to the regular wagon fleet. The wagons are suitable for bulk goods of various kinds. Its five troughs can be tilted to either side and can deliver the bulk material directly into trucks or over bunker walls.",
+        ["Schotterkies165_DBAG_name"] = "Schotter-Kies 165 DBAG (1994).",
+		["Schotterkies165_DBAG_description"] = "From 1991, 400 F-z 120s are re-designated as service freight wagons and given the designation Schotter-Kies 165. From 1994, Deutsche Bahn AG took over the wagons. In 1995, most of these wagons return to the regular wagon fleet. The wagons are suitable for bulk goods of various kinds. Its five troughs can be tilted to both sides and can discharge the bulk material directly into trucks or over bunker walls",
+
+	},
+	de = {
+
+		["mod_name"] = "Zweiachsiger Muldenkippwagen (Ommi 51/F-z 120)",
+		["mod_desc"] = "Ab 1952 wurden fast 4.000 Wagen der Bauart Ommi 51 gebaut, die bis 2004 im Einsatz standen. Die Wagen eignen sich für Schüttgüter verschiedener Art. Seine fünf Mulden sind nach beiden Seiten kippbar und können das Schüttgut direkt in LKW oder über Bunkermauern hinweg abgeben. Die Entladung war schwierig und durfte nur von geschulten Bedienern vorgenommen werden, weil sonst der Wagen umkippen konnte. In den 1990er Jahren waren die Wagen zeitweise als Dienstgüterwagen unterwegs.",
+
+        ["Ommi51_menu"] = "Zweiachsiger Muldenkippwagen",
+
+		["Ommi51_name"] = "Ommi 51 (1952)",
+		["Ommi51_description"] = "Ab 1952 wurden fast 4.000 Wagen der Bauart Ommi 51 angeschafft bei der Bundesbahn. Die Wagen eignen sich für Schüttgüter verschiedener Art. Seine fünf Mulden sind nach beiden Seiten kippbar und können das Schüttgut direkt in LKW oder über Bunkermauern hinweg abgeben.",
+        ["Fz120_name"] = "F-z 120 (1965)",
+		["Fz120_description"] = "Ab 1965 wurden die ehemaligen Ommi 51 Wagen nach RIV-Richtlinie umgezeichnet in F-z 120. Die Wagen eignen sich für Schüttgüter verschiedener Art. Seine fünf Mulden sind nach beiden Seiten kippbar und können das Schüttgut direkt in LKW oder über Bunkermauern hinweg abgeben.",
+        ["Fz120_DBAG_name"] = "F-z 120 DBAG (1994)",
+		["Fz120_DBAG_description"] = "Auch zur Deutschen Bahn AG kamen noch viele Wagen und waren noch bis 2004 im Bestand. Die Wagen eignen sich für Schüttgüter verschiedener Art. Seine fünf Mulden sind nach beiden Seiten kippbar und können das Schüttgut direkt in LKW oder über Bunkermauern hinweg abgeben.",
+        ["Schotterkies165_name"] = "Schotter-Kies 165 (1991)",
+		["Schotterkies165_description"] = "Ab 1991 werden 400 F-z 120 zu Dienstgüterwagen umgezeichnet und erhalten die Bezeichnung Schotter-Kies 165. 1995 kehren diese Wagen überwiegend in den regulären Wagenpark zurück. Die Wagen eignen sich für Schüttgüter verschiedener Art. Seine fünf Mulden sind nach beiden Seiten kippbar und können das Schüttgut direkt in LKW oder über Bunkermauern hinweg abgeben.",
+        ["Schotterkies165_DBAG_name"] = "Schotter-Kies 165 DBAG (1994)",
+		["Schotterkies165_DBAG_description"] = "Ab 1991 werden 400 F-z 120 zu Dienstgüterwagen umgezeichnet und erhalten die Bezeichnung Schotter-Kies 165. Ab 1994 übernahm die Deutsche Bahn AG die Wagen. 1995 kehren diese Wagen überwiegend in den regulären Wagenpark zurück. Die Wagen eignen sich für Schüttgüter verschiedener Art. Seine fünf Mulden sind nach beiden Seiten kippbar und können das Schüttgut direkt in LKW oder über Bunkermauern hinweg abgeben.",
+
+	},
+}
+end
